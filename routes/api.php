d9d7f8137e41f3ba89dcca86314e6f657412b675
Chewienator <cgomez@mpg-live.com>
2020-03-01 10:30:11 +1100

    Implemented authentication api tih JWT-auth

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * Route group for JWT authentication methods
+ */
+Route::group(['prefix'=>'auth', 'namespace'=>'Auth'], function() {
+    Route::post('signin', 'SignInController');
+    Route::post('signout', 'SignOutController');
+    
+    Route::get('me', 'MeController');
 });
